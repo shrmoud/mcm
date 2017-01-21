d5846ebf667109d4912406a2553c159c389fb5fa
Ubuntu <ubuntu@ip-172-31-34-84.us-west-2.compute.internal>
2017-01-21 20:35:00 +0000

    Almost?

--- plot.py
+++ plot.py
@@ -9,8 +9,6 @@ import matplotlib.pyplot as plt
 G=nx.Graph()
 mylist = []
 tuplist = []
-#graph =  {}
-
 
 with open('data.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
@@ -22,32 +20,25 @@ with open('data.csv', 'rb') as csvfile:
    # print len(mylist)
 
 for i in range(len(mylist)):
-    #j = i+1
     mysplit = mylist[i].split(",")
-    #print mysplit
+   #print mysplit
     start = mysplit[1]
     end = mysplit[2]
-    w = int(mysplit[3])
+    #w = int(mysplit[3])
     #print type(w)
     tup = (start,end)
     tuplist.append(tup)
     G.add_edge(start, end)#, weight= (w/1000))
-    #while(j == (len(mylist)):
-          #jstart = (mylist[j].split(","))[1]
-          #jend = (mylist[j].split(","))[2]
 
-          #if(end == jend): graph[start] = ?         
-          #j++
-#print tuplist
+print("Length of tuple list")
+print (len(tuplist))
+                            
 
-                              
-
-
-#print("Nodes of graph: ")
-#print(G.nodes())
-#print("Edges of graph: ")
-#print(G.edges())
+print("Number of Nodes of graph: ")
+print(len(G.nodes()))
+print("Number of Edges of graph: ")
+print(len(G.edges()))
 
 nx.draw(G)
-plt.savefig("weightpath2.png") # save as png
+plt.savefig("mypath2.png") # save as png
 plt.show() # display
