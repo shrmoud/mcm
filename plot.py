92138992d77b5bc8fb6aab96bb5350562a0867fd
Ubuntu <ubuntu@ip-172-31-34-84.us-west-2.compute.internal>
2017-01-21 19:57:16 +0000

    paths

--- plot.py
+++ plot.py
@@ -27,6 +27,7 @@ for i in range(len(mylist)):
     end = (mylist[i].split(","))[2]
     tup = (start,end)
     tuplist.append(tup)
+    G.add_edge(start, end)#, weight=4)
     #while(j == (len(mylist)):
           #jstart = (mylist[j].split(","))[1]
           #jend = (mylist[j].split(","))[2]
@@ -36,21 +37,13 @@ for i in range(len(mylist)):
 #print tuplist
 
                               
-G.add_node("a")
-G.add_nodes_from(["b","c"])
-G.add_edge(1,2)
-edge = ("d", "e")
-G.add_edge(*edge)
-edge = ("a", "b")
-G.add_edge(*edge)
 
-print("Nodes of graph: ")
-print(G.nodes())
-print("Edges of graph: ")
-print(G.edges())
 
-# adding a list of edges:
-G.add_edges_from([("a","c"),("c","d"), ("a",1), (1,"d"), ("a",2)])
+#print("Nodes of graph: ")
+#print(G.nodes())
+#print("Edges of graph: ")
+#print(G.edges())
+
 nx.draw(G)
-plt.savefig("simple_path.png") # save as png
+plt.savefig("path.png") # save as png
 plt.show() # display
