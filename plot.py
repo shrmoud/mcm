5b5099d17d592198c5d72585b58516d0c4a70b83
Ubuntu <ubuntu@ip-172-31-34-84.us-west-2.compute.internal>
2017-01-21 19:44:10 +0000

    Stuf

--- plot.py
+++ plot.py
@@ -1,12 +1,45 @@
 import csv
 
 mylist = []
-tup = ()
+tuplist = []
+#graph =  {}
+
+
 with open('data.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
         mylist += row
-        print ','.join(row)
+        #print ','.join(row)
 
-    print mylist[0:2]
-    print len(mylist)
+   # print mylist[0:2]
+   # print len(mylist)
+
+for i in range(len(mylist)):
+    #j = i+1
+    start = (mylist[i].split(","))[1]
+    end = (mylist[i].split(","))[2]
+    tup = (start,end)
+    tuplist.append(tup)
+    #while(j == (len(mylist)):
+          #jstart = (mylist[j].split(","))[1]
+          #jend = (mylist[j].split(","))[2]
+
+          #if(end == jend): graph[start] = ?         
+          #j++
+print tuplist
+
+
+
+
+'''
+def generate_edges(graph):
+    edges = []
+    for node in graph:
+        for neighbour in graph[node]:
+            edges.append((node, neighbour))
+
+    return edges
+'''
+
+#print(generate_edges(graph))
+                                     
